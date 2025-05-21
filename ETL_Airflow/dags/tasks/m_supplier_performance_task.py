fc4a7c07fd36df4d431658a5cca904c5bf756260
sahithi <sahithigudivada27@gmail.com>
2025-05-21 23:25:00 +0530

    completed the review changes

--- ETL_Airflow/dags/tasks/m_supplier_performance_task.py
+++ ETL_Airflow/dags/tasks/m_supplier_performance_task.py
@@ -2,7 +2,7 @@ from airflow.decorators import task
 from airflow.exceptions import AirflowException
 from utils import init_spark,load_to_postgres,DuplicateValidator,read_from_postgres
 import logging
-from pyspark.sql.functions import col, sum , countDistinct, current_date, row_number,when
+from pyspark.sql.functions import col, sum as _sum  , countDistinct, current_date, row_number,when
 from pyspark.sql.window import Window
 
 log = logging.getLogger(__name__)
@@ -19,22 +19,22 @@ def m_load_supplier_performance():
                                  .select(
                                     col("ORDER_STATUS"),
                                     col("PRODUCT_ID"),
-                                    col("QUANTITY"),
-                                    col("SELLING_PRICE")
+                                    col("QUANTITY")                                  
                                 )        
         log.info(f"Data Frame : 'SQ_Shortcut_To_sales' is built....")
 
-        SQ_Shortcut_To_products = read_from_postgres(spark, "raw.products")
-        SQ_Shortcut_To_products = SQ_Shortcut_To_products \
+        SQ_Shortcut_To_Products = read_from_postgres(spark, "raw.products")
+        SQ_Shortcut_To_Products = SQ_Shortcut_To_Products \
                                      .select(                                      
                                       col("PRODUCT_ID"),
                                       col("SUPPLIER_ID"),
-                                      col("PRODUCT_NAME")                                    
+                                      col("PRODUCT_NAME"),
+                                      col("SELLING_PRICE")                                   
                                     )
         log.info(f"Data Frame : 'SQ_Shortcut_To_products' is built....")
 
-        SQ_Shortcut_To_suppliers = read_from_postgres(spark, "raw.suppliers")
-        SQ_Shortcut_To_suppliers =  SQ_Shortcut_To_suppliers\
+        SQ_Shortcut_To_Suppliers = read_from_postgres(spark, "raw.suppliers")
+        SQ_Shortcut_To_Suppliers =  SQ_Shortcut_To_Suppliers\
                                     .select(
                                         col("SUPPLIER_ID"),
                                         col("SUPPLIER_NAME")
@@ -44,13 +44,23 @@ def m_load_supplier_performance():
         FIL_Cancelled_Sales = SQ_Shortcut_To_sales.filter(col("ORDER_STATUS") != "CANCELLED")
 
         
-        JNR_Sales_Products = FIL_Cancelled_Sales.join(
-            SQ_Shortcut_To_products, on="PRODUCT_ID", how="inner"
+        JNR_Sales_Products = FIL_Cancelled_Sales.select(
+            "PRODUCT_ID", "QUANTITY"
+        ).join(
+        SQ_Shortcut_To_Products.select("PRODUCT_ID", "SUPPLIER_ID", "PRODUCT_NAME"),
+            on="PRODUCT_ID",
+            how="inner"
         )
-        JNR_Products_Suppliers = JNR_Sales_Products.join(
-            SQ_Shortcut_To_suppliers, on="SUPPLIER_ID", how="inner"
+
+        # Join with suppliers with selected columns
+        JNR_Products_Suppliers = JNR_Sales_Products.select(
+            "PRODUCT_ID", "PRODUCT_NAME", "SUPPLIER_ID", "QUANTITY"
+        ).join(
+            SQ_Shortcut_To_Suppliers.select("SUPPLIER_ID", "SUPPLIER_NAME"),
+            on="SUPPLIER_ID",
+            how="inner"
         )
-      
+
        
         JNR_Products_Suppliers = JNR_Products_Suppliers.withColumn(
             "REVENUE", col("QUANTITY") * col("SELLING_PRICE")
@@ -60,29 +70,29 @@ def m_load_supplier_performance():
         AGG_TRANS_Product_Level = JNR_Products_Suppliers.groupBy(
             "SUPPLIER_ID", "PRODUCT_ID", "PRODUCT_NAME"
         ).agg(
-            sum("REVENUE").alias("agg_product_revenue"),
-            sum("QUANTITY").alias("agg_stock_sold")
+            _sum("REVENUE").alias("agg_product_revenue"),
+            _sum("QUANTITY").alias("agg_stock_sold")
         )
         
        
         AGG_TRANS_Supplier_Level = AGG_TRANS_Product_Level.groupBy("SUPPLIER_ID").agg(
-            sum("agg_product_revenue").alias("agg_total_revenue"),
+            _sum("agg_product_revenue").alias("agg_total_revenue"),
             countDistinct("PRODUCT_ID").alias("agg_total_products_sold"),
-            sum("agg_stock_sold").alias("agg_total_stock_sold")
+            _sum("agg_stock_sold").alias("agg_total_stock_sold")
         )
 
        
         windowSpec = Window.partitionBy("SUPPLIER_ID").orderBy(col("agg_product_revenue").desc())
-        ranked_df = AGG_TRANS_Product_Level.withColumn("RANK", row_number().over(windowSpec))
+        RNK_Suppliers_df = AGG_TRANS_Product_Level.withColumn("RANK", row_number().over(windowSpec))
 
-        top_selling_product_df = ranked_df.filter(col("RANK") == 1).select(
+        Top_Selling_Product_df = RNK_Suppliers_df.filter(col("RANK") == 1).select(
             "SUPPLIER_ID", col("PRODUCT_NAME").alias("TOP_SELLING_PRODUCT")
         )
 
-        final_result = SQ_Shortcut_To_suppliers.join(
+        JNR_Supplier_Aggregates_And_Top_Product = SQ_Shortcut_To_Suppliers.join(
             AGG_TRANS_Supplier_Level, on="SUPPLIER_ID", how="left"
         ).join(
-            top_selling_product_df, on="SUPPLIER_ID", how="left"
+            Top_Selling_Product_df, on="SUPPLIER_ID", how="left"
         ).fillna(0, subset=[
             "agg_total_revenue", "agg_total_products_sold", "agg_total_stock_sold"
         ]).withColumn(
@@ -93,7 +103,7 @@ def m_load_supplier_performance():
             ).otherwise(col("TOP_SELLING_PRODUCT"))
         ).withColumn("DAY_DT", current_date())
 
-        Shortcut_To_supplier_performance_tgt = final_result.select(
+        Shortcut_To_Supplier_Performance_Tgt = JNR_Supplier_Aggregates_And_Top_Product.select(
                                                 col("DAY_DT"),
                                                 col("SUPPLIER_ID"),
                                                 col("SUPPLIER_NAME"),
@@ -105,10 +115,10 @@ def m_load_supplier_performance():
 
         
         validator = DuplicateValidator()
-        validator.validate_no_duplicates(Shortcut_To_supplier_performance_tgt,key_columns=["SUPPLIER_ID", "DAY_DT"] )
+        validator.validate_no_duplicates(Shortcut_To_Supplier_Performance_Tgt,key_columns=["SUPPLIER_ID", "DAY_DT"] )
 
       
-        load_to_postgres(Shortcut_To_supplier_performance_tgt,"legacy.supplier_performance","append")   
+        load_to_postgres(Shortcut_To_Supplier_Performance_Tgt,"legacy.supplier_performance","append")   
 
         return "Supplier Performance task finished."
 
