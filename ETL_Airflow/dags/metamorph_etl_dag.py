c4b7e215ff18039638f16d5a85e170cb63eb13fa
sahithi <sahithigudivada27@gmail.com>
2025-05-22 17:07:48 +0530

    selecting only needed columns in tables

--- ETL_Airflow/dags/metamorph_etl_dag.py
+++ ETL_Airflow/dags/metamorph_etl_dag.py
@@ -10,14 +10,15 @@ from tasks.m_supplier_performance_task import m_load_supplier_performance
     catchup=False,
 )
 def etl_process():
-    supplier_task = m_ingest_data_into_suppliers()
-    product_task = m_ingest_data_into_products()
-    customer_task = m_ingest_data_into_customers()
-    sales_task = m_ingest_data_into_sales()
+    # supplier_task = m_ingest_data_into_suppliers()
+    # product_task = m_ingest_data_into_products()
+    # customer_task = m_ingest_data_into_customers()
+    # sales_task = m_ingest_data_into_sales()
     supplier_performance_task = m_load_supplier_performance()
 
     # Set dependencies inside the DAG function
-    [ supplier_task , product_task , customer_task , sales_task ]  >> supplier_performance_task
+   
+    supplier_performance_task
   
 
 dag_instance = etl_process()
--- ETL_Airflow/dags/tasks/m_supplier_performance_task.py
+++ ETL_Airflow/dags/tasks/m_supplier_performance_task.py
@@ -13,7 +13,7 @@ def m_load_supplier_performance():
     try:
         spark = init_spark()
 
-        #processing node SQ_Shortcut_To_sales -reads data from sales table
+        # Processing Node : SQ_Shortcut_To_sales -reads data from sales table
         SQ_Shortcut_To_sales = read_from_postgres(spark, "raw.sales")
         SQ_Shortcut_To_sales = SQ_Shortcut_To_sales\
                                  .select(
@@ -40,47 +40,62 @@ def m_load_supplier_performance():
                                         col("SUPPLIER_NAME")
                                     )
         log.info(f"Data Frame : 'SQ_Shortcut_To_suppliers' is built....")
-       
-        FIL_Cancelled_Sales = SQ_Shortcut_To_sales.filter(col("ORDER_STATUS") != "CANCELLED")
-
         
-        JNR_Sales_Products = FIL_Cancelled_Sales.select(
-            "PRODUCT_ID", "QUANTITY"
-        ).join(
-        SQ_Shortcut_To_Products.select("PRODUCT_ID", "SUPPLIER_ID", "PRODUCT_NAME"),
-            on="PRODUCT_ID",
-            how="inner"
-        )
-
+       
+        FIL_Cancelled_Sales = SQ_Shortcut_To_sales.filter(SQ_Shortcut_To_sales.ORDER_STATUS != "CANCELLED")
+        log.info(f"Data Frame : 'FIL_Cancelled_Sales' is built....")
+        
+        
+        JNR_Sales_Products = FIL_Cancelled_Sales\
+                                .join( 
+                                    SQ_Shortcut_To_Products, 
+                                    on="PRODUCT_ID",
+                                    how="inner"
+                                )\
+                                .select(
+                                    SQ_Shortcut_To_sales.QUANTITY,
+                                    SQ_Shortcut_To_Products.PRODUCT_ID, 
+                                    SQ_Shortcut_To_Products.SUPPLIER_ID,
+                                    SQ_Shortcut_To_Products.PRODUCT_NAME,
+                                    SQ_Shortcut_To_Products.SELLING_PRICE
+                                )       
+        log.info(f"Data Frame : 'JNR_Sales_Products' is built....")
+        
         # Join with suppliers with selected columns
-        JNR_Products_Suppliers = JNR_Sales_Products.select(
-            "PRODUCT_ID", "PRODUCT_NAME", "SUPPLIER_ID", "QUANTITY"
-        ).join(
-            SQ_Shortcut_To_Suppliers.select("SUPPLIER_ID", "SUPPLIER_NAME"),
-            on="SUPPLIER_ID",
-            how="inner"
-        )
-
-       
-        JNR_Products_Suppliers = JNR_Products_Suppliers.withColumn(
-            "REVENUE", col("QUANTITY") * col("SELLING_PRICE")
-        )
-
-        
+        JNR_Products_Suppliers = JNR_Sales_Products\
+                                    .join(
+                                        SQ_Shortcut_To_Suppliers,
+                                        on="SUPPLIER_ID",
+                                        how="inner"
+                                    )\
+                                    .select(
+                                        JNR_Sales_Products.PRODUCT_ID,
+                                        JNR_Sales_Products.PRODUCT_NAME,
+                                        JNR_Sales_Products.QUANTITY,
+                                        JNR_Sales_Products.SELLING_PRICE,
+                                        SQ_Shortcut_To_Suppliers.SUPPLIER_ID,
+                                        SQ_Shortcut_To_Suppliers.SUPPLIER_NAME  
+                                     ).withColumn(
+                                        "REVENUE", col("QUANTITY") * col("SELLING_PRICE")
+                                    )        
+        log.info(f"Data Frame : 'JNR_Products_Suppliers' is built....")                             
+                                   
+                               
+                                           
         AGG_TRANS_Product_Level = JNR_Products_Suppliers.groupBy(
             "SUPPLIER_ID", "PRODUCT_ID", "PRODUCT_NAME"
         ).agg(
             _sum("REVENUE").alias("agg_product_revenue"),
             _sum("QUANTITY").alias("agg_stock_sold")
         )
-        
+        log.info(f"Data Frame : 'AGG_TRANS_Product_Level' is built....")
        
         AGG_TRANS_Supplier_Level = AGG_TRANS_Product_Level.groupBy("SUPPLIER_ID").agg(
             _sum("agg_product_revenue").alias("agg_total_revenue"),
             countDistinct("PRODUCT_ID").alias("agg_total_products_sold"),
             _sum("agg_stock_sold").alias("agg_total_stock_sold")
         )
-
+        log.info(f"Data Frame : 'AGG_TRANS_Supplier_Level' is built....")
        
         windowSpec = Window.partitionBy("SUPPLIER_ID").orderBy(col("agg_product_revenue").desc())
         RNK_Suppliers_df = AGG_TRANS_Product_Level.withColumn("RANK", row_number().over(windowSpec))
@@ -88,6 +103,7 @@ def m_load_supplier_performance():
         Top_Selling_Product_df = RNK_Suppliers_df.filter(col("RANK") == 1).select(
             "SUPPLIER_ID", col("PRODUCT_NAME").alias("TOP_SELLING_PRODUCT")
         )
+        log.info(f"Data Frame : 'Top_Selling_Product_df' is built....")
 
         JNR_Supplier_Aggregates_And_Top_Product = SQ_Shortcut_To_Suppliers.join(
             AGG_TRANS_Supplier_Level, on="SUPPLIER_ID", how="left"
@@ -102,6 +118,7 @@ def m_load_supplier_performance():
                 "No Sales"
             ).otherwise(col("TOP_SELLING_PRODUCT"))
         ).withColumn("DAY_DT", current_date())
+        log.info(f"Data Frame : 'JNR_Supplier_Aggregates_And_Top_Product' is built....")
 
         Shortcut_To_Supplier_Performance_Tgt = JNR_Supplier_Aggregates_And_Top_Product.select(
                                                 col("DAY_DT"),
@@ -112,7 +129,7 @@ def m_load_supplier_performance():
                                                 col("agg_total_stock_sold").alias("TOTAL_STOCK_SOLD"),
                                                 col("TOP_SELLING_PRODUCT")
                                               )
-
+        log.info(f"Data Frame : 'Shortcut_To_Supplier_Performance_Tgt' is built....")
         
         validator = DuplicateValidator()
         validator.validate_no_duplicates(Shortcut_To_Supplier_Performance_Tgt,key_columns=["SUPPLIER_ID", "DAY_DT"] )
@@ -127,4 +144,7 @@ def m_load_supplier_performance():
         raise AirflowException(f"Supplier_performance ETL failed: {str(e)}")
 
     finally:
-        spark.stop()
+        spark.stop()     
+                                                                                                          
+                              
+        
