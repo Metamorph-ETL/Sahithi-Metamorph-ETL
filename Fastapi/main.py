e8db438e0af3f6f5d279083d46f4178bf1b566a8
sahithi <sahithigudivada27@gmail.com>
2025-08-04 20:15:17 +0530

    Fix v1 API endpoints to use today's date when fetching latest files from GCS,

--- Fastapi/main.py
+++ Fastapi/main.py
@@ -63,7 +63,45 @@ def read_csv_from_gcs(blob):
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"CSV read error: {str(e)}")
 
-# API Endpoints
+
+# API Endpoints for v1
+@app.post("/token", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token = create_access_token(
+        data={"sub": user.username},
+        expires_delta=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES),
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+@app.get("/v1/products")
+def get_products():
+    date = datetime.now().strftime("%Y%m%d")
+    blob = get_latest_file_from_gcs("product",date) 
+    df = read_csv_from_gcs(blob)
+    return {"status": 200, "data": df.to_dict(orient="records")}
+  
+@app.get("/v1/customers")
+def get_customers(current_user: User = Depends(get_current_active_user)):
+    date = datetime.now().strftime("%Y%m%d")
+    blob = get_latest_file_from_gcs("customer",date)
+    df = read_csv_from_gcs(blob)
+    return {"status": 200, "data": df.to_dict(orient="records")}
+
+@app.get("/v1/suppliers")
+def get_suppliers():
+    date = datetime.now().strftime("%Y%m%d")
+    blob = get_latest_file_from_gcs("supplier",date)
+    df = read_csv_from_gcs(blob)
+    return {"status": 200, "data": df.to_dict(orient="records")}
+
+# API Endpoints for v2
 @app.post("/v1/token", response_model=Token)
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
     user = authenticate_user(fake_users_db, form_data.username, form_data.password)
